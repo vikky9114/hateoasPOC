c07db461496a9195dbb6770ac9c4877fc38dd9cd
Vivek <Vivek@DESKTOP-R7NHVJK>
2020-09-30 14:33:34 +0530

    Added repo and code base

---
+++ src/main/java/com/hateoas/payment/HateoasPocApplication.java
@@ -0,0 +1,13 @@
+package com.hateoas.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hateoas/payment/HateoasPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hateoas.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
